1c8aaabba2b95e5ef6392bd5bbe526a35616cd29
Vinitha Fernando <vfernandomannar1999@gmail.com>
2022-04-28 15:44:41 +0530

    Create user model

---
+++ packages/server/src/models/user.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    first: {
+      type: String,
+      required: true,
+    },
+    last: {
+      type: String,
+      required: true,
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "manager", "supervisor"],
+    required: true,
+  },
+  managedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+});
+
+userSchema.pre("save", function (next) {
+  if (this.isModified("password")) {
+    bcrypt.hash(this.password, 8, (err, hash) => {
+      if (err) return next(err);
+
+      this.password = hash;
+      next();
+    });
+  }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  if (!password) throw new Error("Password is missing, can not compare!");
+
+  try {
+    const result = await bcrypt.compare(password, this.password);
+    return result;
+  } catch (error) {
+    console.log("Error while comparing password!", error.message);
+  }
+};
+
+userSchema.statics.isThisEmailInUse = async function (email) {
+  if (!email) throw new Error("Invalid Email");
+  try {
+    const user = await this.findOne({ email });
+    if (user) return false;
+
+    return true;
+  } catch (error) {
+    console.log("error inside isThisEmailInUse method", error.message);
+    return false;
+  }
+};
+
+module.exports = mongoose.model("User", userSchema);
