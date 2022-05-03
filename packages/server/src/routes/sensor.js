9e9ff4f68e8a2251c267680560bc85aab706ae24
Shrikarsan Logeswaran <shrikarsan@gmail.com>
2022-05-04 00:02:26 +0530

    Add sensor routes

---
+++ packages/server/src/routes/sensor.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getSensors,
+  addSensor,
+  deleteSensor,
+} = require("../controllers/sensor");
+
+router.route("/sensor").get(getSensors).post(addSensor);
+
+router.route("/sensor/:id").delete(deleteSensor);
+
+module.exports = router;
