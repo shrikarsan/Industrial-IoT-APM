1c12a28632e474e28fb7e2680d37d7582e98a017
Zamhana Faiz <fathimazamhana123@gmail.com>
2022-05-08 23:58:41 +0530

    Initialze login page

--- packages/mobile/src/navigation/index.js
+++ packages/mobile/src/navigation/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import Sample from '../views/Sample';
+import Login from '../views/Login';
 
 const Stack = createNativeStackNavigator();
 const StackNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Login" component={Sample} />
+      <Stack.Screen name="Login" component={Login} />
     </Stack.Navigator>
   );
 };
